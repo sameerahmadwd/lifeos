fa13d4e74f8ba9d414b92f65a75c3aa64592ce34
Sameer Ahmad <sameerahmadsaifi@gmail.com>
2026-03-28 12:58:54 +0530

    added console

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+console.log('API URL:', import.meta.env.VITE_API_URL);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
